290c95fc7a93b927f46e7393b8ceaf2e34db2990
Reddogstone <thereddog@gmx.net>
2016-08-11 10:13:16 +0200

    Added some basic routing Added .gitignore

--- index.js
+++ index.js
@@ -3,12 +3,17 @@
 const http = require('http');
 const { stack, finalizer, route } = require('yokto');
 const Static = require('node-static');
+const mkdirp = require('mkdirp');
 
 const file = new Static.Server('./public');
 
 const hostname = '127.0.0.1';
 const port = 8000;
 
+mkdirp.sync('db');
+
 http.createServer(stack([
-	request => response => file.serve(request, response)
+	route.get('/', request => finalizer.redirect('/index.html')),
+	route.get('/index.html', request => response => file.serve(request, response)),
+	route.regex(/css$/, request => response => file.serve(request, response))
 ])).listen(port, hostname, () => console.log(`Server started: ${hostname}:${port}`));
