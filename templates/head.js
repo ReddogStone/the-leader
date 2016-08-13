efcfaf5a39d8e39d6b91ebf7f1d4283b7b342483
ReddogStone <thereddog@gmx.net>
2016-08-13 13:04:52 +0200

    Whitespace

--- templates/head.js
+++ templates/head.js
@@ -4,6 +4,6 @@ module.exports = `
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 	<link rel="stylesheet" href="/main.css">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta charset="UTF-8"> 
+	<meta charset="UTF-8">
 </head>
 `;
